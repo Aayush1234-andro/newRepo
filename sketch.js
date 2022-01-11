f3e3d1a226121ce78db9f4077cf120038d504184
Aayush1234-andro <79866727+Aayush1234-andro@users.noreply.github.com>
2022-01-11 20:44:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var bgImage
+var soilder
+var sfImg
+var sbImg
+var slImg
+var srImg
+var sReachImg
+
+
+function preload(){
+    bgImage = loadImage("assets/militaryBase.jpg");
+    sfImg = loadImage("assets/soliderfront.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1940,900);
+    //background(bgImage);
+    soilder = createSprite(20,200, 50, 50);
+    soilder.addImage(sfImg)
+    soilder.scale = 0.3
+    soilder.setCollider("rectangle",0,0,300,300)
+   
+}
+
+function draw(){
+    
+}
